eb73854fb0ebcc8fb986e0924ac82f48cbc845e1
smitford <jump145@mail.ru>
2023-04-10 16:52:07 +0400

    Sprint #9

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,10 +1,15 @@
 package com.example.playlistmaker
 
 import android.annotation.SuppressLint
+import android.app.SearchManager
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Im
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat.IntentBuilder
 
 class SettingsActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -12,10 +17,38 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        val shearButton = findViewById<ImageView>(R.id.shear_button)
+
+        shearButton.setOnClickListener {
+            val shearIntent = Intent(Intent.ACTION_SEND)
+            shearIntent.data = Uri.parse("")
+            shearIntent.type = "text/html"
+            shearIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.shear_app))
+            startActivity(shearIntent)
+
+        }
+
+        val supportButton = findViewById<ImageView>(R.id.support_button)
+        supportButton.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SEND)
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.student_mail)))
+            supportIntent.putExtra(Intent.EXTRA_TITLE, getString(R.string.title_of_sup_mail))
+            supportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.text_of_sup_mail))
+            startActivity(supportIntent)
+
+        }
+        val termsOfUseButton = findViewById<ImageView>(R.id.terms_of_use_button)
+        termsOfUseButton.setOnClickListener {
+
+            val termsOfUseIntent = Intent(Intent.ACTION_WEB_SEARCH)
+            termsOfUseIntent.putExtra(SearchManager.QUERY, getString(R.string.term_of_use_web) )
+            startActivity(termsOfUseIntent)
+        }
         val backButton = findViewById<Button>(R.id.back_button)
         backButton.setOnClickListener {
-            val mainActivityDisplay = Intent(this, MainActivity::class.java)
-            startActivity(mainActivityDisplay)
+            this.finish()
         }
     }
+
 }
