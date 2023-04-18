74e61e12352efa5dcb5228c9f9f13b8d4973fe47
smitford <jump145@mail.ru>
2023-04-18 21:09:41 +0400

    Sprint #10 Home Work

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -7,20 +8,30 @@ import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.playlistmaker.trackRecycleView.CustomRecyclerAdapter
+import com.example.playlistmaker.trackRecycleView.Track
+
 
 class SearchActivity : AppCompatActivity() {
 
-    var inputEditText: EditText? = null
-
+    lateinit var inputEditText: EditText
     var textSearch: String = String()
 
+
     companion object {
-        const val textOfSearch = "textOfSearch"
+        const val textOfSearch = "TEXT_OF_SEARCH"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val recyclerViewSongs: RecyclerView = findViewById(R.id.recyclerView_songs)
+
+        recyclerViewSongs.layoutManager = LinearLayoutManager(this)
+        recyclerViewSongs.adapter = CustomRecyclerAdapter(fillList())
 
         val buttonBack = findViewById<Button>(R.id.back_button_search_act)
         buttonBack.setOnClickListener {
@@ -30,14 +41,14 @@ class SearchActivity : AppCompatActivity() {
         inputEditText = findViewById(R.id.search_bar)
 
         if (savedInstanceState != null) {
-            inputEditText!!.setText(textSearch)
+            inputEditText.setText(textSearch)
         }
 
 
         val clearButton = findViewById<Button>(R.id.clear_text_search)
 
         clearButton.setOnClickListener {
-            inputEditText!!.setText("")
+            inputEditText.setText("")
         }
 
         val searchActivityTextWatcher = object : TextWatcher {
@@ -59,9 +70,22 @@ class SearchActivity : AppCompatActivity() {
                 }
             }
         }
-        inputEditText!!.addTextChangedListener(searchActivityTextWatcher)
+        inputEditText.addTextChangedListener(searchActivityTextWatcher)
 
 
+    }
+
+    private fun fillList(): List<Track>{
+        val data = mutableListOf<Track>()
+        val track = this.resources.getStringArray(R.array.tracks)
+        val trackTime = this.resources.getStringArray(R.array.trackTime)
+        val artistName = this.resources.getStringArray(R.array.artistName)
+        val artworkUrl = this.resources.getStringArray(R.array.artworkUrl)
+
+        for (i in 0..4){
+            data.add(Track(trackName = track[i], artistName = artistName[i], trackTime = trackTime[i], artworkUrl100 = artworkUrl[i]))
+        }
+        return data
     }
 
 
@@ -73,7 +97,7 @@ class SearchActivity : AppCompatActivity() {
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        textSearch = savedInstanceState.getString(textOfSearch, "не работает")
+        textSearch = savedInstanceState.getString(textOfSearch, "")
     }
 
 
---
+++ app/src/main/java/com/example/playlistmaker/trackRecycleView/Track.kt
@@ -0,0 +1,9 @@
+package com.example.playlistmaker.trackRecycleView
+
+class Track(
+    val trackName: String,
+    val artistName: String,
+    val trackTime: String,
+    val artworkUrl100: String
+) {
+}
