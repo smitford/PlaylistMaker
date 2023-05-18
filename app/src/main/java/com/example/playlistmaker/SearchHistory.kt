4c7a5f7595676fc807823167c75f3493d166974e
smitford <jump145@mail.ru>
2023-05-18 18:40:18 +0400

    Sprint #12 Home Work after 1st review, time change

---
+++ app/src/main/java/com/example/playlistmaker/SearchHistory.kt
@@ -0,0 +1,85 @@
+package com.example.playlistmaker
+
+import android.content.SharedPreferences
+import com.example.playlistmaker.trackRecycleView.Track
+import com.google.gson.Gson
+
+object SearchHistory {
+    lateinit var sharedPreferencesHistory: SharedPreferences
+
+    private val gson = Gson()
+
+    var history: String? = ""
+
+    private var historyList = mutableListOf<Track>()
+
+    fun notEmpty () : Boolean = historyList.any()
+
+    private fun getTracksFromHistory () :MutableList<Track> =
+       gson.fromJson(
+            history.toString(),
+            Array<Track>::class.java,
+        )
+            .toMutableList()
+
+    fun clearHistory () {
+        sharedPreferencesHistory.edit().remove(SEARCH_HISTORY).apply()
+        historyList.clear()
+    }
+
+    fun refreshHistory () {
+        history = sharedPreferencesHistory.getString(SEARCH_HISTORY, "")
+        historyList = if(history!="") getTracksFromHistory() else mutableListOf()
+    }
+
+    fun getHistory(): MutableList<Track> = historyList
+
+
+    private fun checkForCoincidence(track: Track): Boolean {
+        historyList.forEach { if (it.trackId==track.trackId) return true }
+        return false
+    }
+
+    private fun addTrack(track: Track) {
+        historyList.apply {
+            this.reverse()
+            this.add(track)
+            this.reverse()
+        }
+
+    }
+
+    private fun checkLength(): Boolean = this.historyList.count() < 10
+
+    private fun addToSearchHistory(track: Track) {
+
+        if (checkForCoincidence(track)) {
+            historyList.remove(track)
+            addTrack(track)
+            return
+        }
+
+        if (checkLength()) {
+            addTrack(track)
+        } else {
+            historyList.removeAt(9)
+            addTrack(track)
+        }
+    }
+
+    private fun getJSON(): String = gson.toJson(historyList)
+
+
+    fun saveHistory(track: Track) {
+
+        addToSearchHistory(track)
+        sharedPreferencesHistory.edit().putString(SEARCH_HISTORY, getJSON()).apply()
+
+    }
+
+
+}
+
+
+
+
--- app/src/main/java/com/example/playlistmaker/trackRecycleView/ViewHolderOfSongs.kt
+++ app/src/main/java/com/example/playlistmaker/trackRecycleView/ViewHolderOfSongs.kt
@@ -1,8 +1,8 @@
 package com.example.playlistmaker.trackRecycleView
 
-import android.view.LayoutInflater
+
 import android.view.View
-import android.view.ViewGroup
+
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -10,37 +10,23 @@ import com.example.playlistmaker.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
-class CustomRecyclerAdapter(private val tracks: List<Track>) :
-    RecyclerView.Adapter<CustomRecyclerAdapter.ViewHolderOfSongs>() {
-    class ViewHolderOfSongs(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val albumImg: ImageView = itemView.findViewById(R.id.album_image)
-        private val artistName: TextView = itemView.findViewById(R.id.artist_name)
-        private val trackName: TextView = itemView.findViewById(R.id.song_name)
-        private val trackTime: TextView = itemView.findViewById(R.id.song_length)
 
-        fun bind(track: Track) {
-            trackName.text = track.trackName
-            trackTime.text = track.trackTimeMillis
-            artistName.text = track.artistName
-            Glide.with(albumImg)
-                .load(track.artworkUrl100)
-                .placeholder(R.drawable.placeholder)
-                .transform(RoundedCorners(2))
-                .into(albumImg)
-        }
+class ViewHolderOfSongs(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val albumImg: ImageView = itemView.findViewById(R.id.album_image)
+    private val artistName: TextView = itemView.findViewById(R.id.artist_name)
+    private val trackName: TextView = itemView.findViewById(R.id.song_name)
+    private val trackTime: TextView = itemView.findViewById(R.id.song_length)
 
+    fun bind(track: Track) {
+        trackName.text = track.trackName
+        trackTime.text = track.trackTimeMillis
+        artistName.text = track.artistName
+        Glide.with(albumImg)
+            .load(track.artworkUrl100)
+            .placeholder(R.drawable.placeholder)
+            .transform(RoundedCorners(2))
+            .into(albumImg)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderOfSongs {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.trackrecycleview_item, parent, false)
-        return ViewHolderOfSongs(itemView)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolderOfSongs, position: Int) {
-
-        holder.bind(tracks[position])
-    }
-
-    override fun getItemCount() = tracks.size
 }
+
